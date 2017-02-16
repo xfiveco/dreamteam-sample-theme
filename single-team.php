d3dd66ef8a54eb3e22d6d9a64789141e91e82167
Lubos Kmetko <lubos@xhtmlized.com>
2017-02-16 09:15:14 +0100

    Using globally defined post

--- single-team.php
+++ single-team.php
@@ -1,23 +1,20 @@
 <?php
 /**
- * The Template for displaying single team member (person)
+ * The Template for displaying single team page
  *
  * @package dreamteam
  */
 
 $context = Timber::get_context();
 
-$person = Timber::query_post();
-$context['person'] = $person;
-
 // Get posts of the associated user
-$user = $person->get_field('team_user');
+$user = $context['post']->get_field('team_user');
 if ($user) {
 	$context['user_posts'] = Timber::get_posts(array('author' => $user['ID'], 'post_type' => 'post', 'numberposts' => 3 ));
 }
 
-// Get person's interview
-$interview = $person->get_field('team_interview');
+// Get user's interview
+$interview = $context['post']->get_field('team_interview');
 if ($interview) {
 	$context['interview'] = new TimberPost($interview);
 }
